ab2037f1316905d8b245ecb2a89ca443d5a9cc25
SEGVeenstra <seg.veenstra@gmail.com>
2026-01-09 21:04:22 +0100

    Update logging

--- proximitreats_app/lib/utils/logging.dart
+++ proximitreats_app/lib/utils/logging.dart
@@ -1,11 +1,18 @@
+import 'dart:developer' as developer;
+
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 void setupLogging() {
   Logger.root.level = kDebugMode ? Level.ALL : Level.WARNING;
   Logger.root.onRecord.listen((record) {
-    debugPrint(
-      '[${record.level.name}] ${record.time}: ${record.loggerName}: ${record.message}',
+    developer.log(
+      record.message,
+      name: record.loggerName,
+      error: record.error,
+      stackTrace: record.stackTrace,
+      level: record.level.value,
+      time: record.time,
     );
   });
 }
